c492709e3453b8571d74074b937e3556c6981210
Yvanna Harris <yvannaharris@yvannas-air.attlocal.net>
2017-07-10 18:56:32 -0400

    Pseudocode

---
+++ assets/javascript/app.js
@@ -0,0 +1,23 @@
+//Show start button
+
+//Hide trivia form
+
+//Start button click event
+
+//Hide start button
+
+//Show trivia form
+
+//Start timer
+
+//Display trivia questions and answer radio buttons
+
+//Save input from radio buttons
+
+//When timer ends, hide form
+
+//Compare answers with input
+
+//Show results
+
+//When "finish" button is clicked compare answers and show results
