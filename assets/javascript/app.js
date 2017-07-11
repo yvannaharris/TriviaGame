6f3a7a024f4677ddbb8bce53914bc48d811e1428
Yvanna Harris <yvannaharris@yvannas-air.attlocal.net>
2017-07-10 22:18:04 -0400

    Added semi-working timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,17 +9,80 @@ $(function() {
 		//Hide start button
 		$button.hide();
 	});
+
+	$button.click(stopwatch.start);
 });
 
 
-
-
-
-
-
 //Start timer
+//function timer()	{
+//	clearTimeout(timeout);
+
+//	var timeout = setTimeout(function(){
+//		$('form').hide();
+//	}, 1000 * 60);
+//};
+
+var intervalId;
+
+//prevents the clock from being sped up unnecessarily
+var clockRunning = false;
+
+//  Our stopwatch object.
+var stopwatch = {
+
+  time: 60,
+
+  start: function() {
+
+      if (!clockRunning) {
+        intervalId = setInterval(stopwatch.count, 1000);
+       clockRunning = true;
+      }
+
+  },
+  stop: function() {
+  	
+  		clearInterval(intervalId);
+    	clockRunning = false;	
+  },
+
+  count: function() {
+
+        stopwatch.time--;
+
+        var converted = stopwatch.timeConverter(stopwatch.time);
+
+        $("#time-clock").html(converted);
+  },
+
+  timeConverter: function(t) {
+
+    var minutes = Math.floor(t / 60);
+    var seconds = t - (minutes * 60);
+
+    if (seconds < 10) {
+      seconds = "0" + seconds;
+    }
+
+    if (minutes === 0) {
+      minutes = "00";
+    }
+
+    else if (minutes < 10) {
+      minutes = "0" + minutes;
+    }
+
+    return minutes + ":" + seconds;
+  }
+
+};
+
+//Display timer
+
 
 //Display trivia questions and answer radio buttons
+
 
 //Save input from radio buttons
 
